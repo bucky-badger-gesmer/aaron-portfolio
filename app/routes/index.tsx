3fa9536c4b3b4992e18729ccb57c89ba545e2f78
bucky-badger-gesmer <ajgesmer@comcast.net>
2022-08-18 15:57:16 -0500

    import Hero component in index.tsx

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,24 +1,12 @@
-import { useState } from "react";
 import Nav from '../components/Nav';
-
-import { faRocket } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-
+import Hero from '../components/Hero';
 
 export default function Index() {
-  const [count, setCount] = useState(0);
 
   return (
     <div className="font-['Poppins']">
       <Nav />
-      <div>
-        <h1 className="text-3xl font-bold underline text-green-400">
-          Hi! My name is Aaron!
-          <br />
-          <button onClick={() => setCount(count + 1)}>Click Me</button>:{" "}
-          <>{count}</>
-        </h1>
-      </div>
+      <Hero />
     </div>
   );
 }
