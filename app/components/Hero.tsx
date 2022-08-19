0cecc28ba65887fc26fa7b99c6172e9d8307d2b7
bucky-badger-gesmer <ajgesmer@comcast.net>
2022-08-19 13:06:29 -0500

    connect links

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,10 +1,11 @@
 import { Typewriter } from "react-simple-typewriter";
+import { AiFillLinkedin, AiFillGithub } from "react-icons/ai";
 
 const Hero = () => {
   return (
-    <div className="max-w-[800px] h-screen mx-auto text-center flex flex-col">
-      <h1 className="md: text-7xl sm:text-6xl text-4xl font-bold md:py-6">
-        Hello there! My name is <span className="text-red-500">Aaron!</span>
+    <div className="max-w-[800px] h-screen mx-auto mt-4 text-center flex flex-col">
+      <h1 className="md:text-7xl sm:text-6xl text-4xl font-bold md:py-6">
+        Hello there! My name is <span className="text-red-500">Aaron</span>.
       </h1>
       <p className="font-bold p-2">
         I'm a front end software developer, and I love all things related to the
@@ -29,6 +30,18 @@ const Hero = () => {
           />
         </p>
       </div>
+      <h1 className="mt-10 md:text-5xl sm:text-4xl text-2xl font-bold md:py-6">
+        Let's Connect!
+      </h1>
+      {/* <img src="app/components/profile_pic.png" alt="profile" /> */}
+      <ul className="flex mx-auto jusitfy-center">
+        <li className="hover:cursor-pointer hover:text-red-500 mx-8">
+          <AiFillLinkedin size={40} />
+        </li>
+        <li className="hover:cursor-pointer hover:text-red-500 mx-8">
+          <AiFillGithub size={40} />
+        </li>
+      </ul>
     </div>
   );
 };
