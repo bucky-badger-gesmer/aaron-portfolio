14bdad011df851332029c6ceab608ea68e056e43
bucky-badger-gesmer <ajgesmer@comcast.net>
2022-08-19 10:45:45 -0500

    hero uses typewriter

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,31 +1,32 @@
-import Typed from "react-typed";
+import { Typewriter } from "react-simple-typewriter";
 
 const Hero = () => {
   return (
     <div className="max-w-[800px] h-screen mx-auto text-center flex flex-col">
-      <p className="font-bold p-2">Hi! My name is Aaron!</p>
       <h1 className="md: text-7xl sm:text-6xl text-4xl font-bold md:py-6">
-        Welcome to my website
+        Hello there! My name is <span className="text-red-500">Aaron!</span>
       </h1>
+      <p className="font-bold p-2">
+        I'm a front end software developer, and I love all things related to the
+        web!
+      </p>
       <div className="flex justify-center items-center md:text-5xl sm:text-4xl text-xl font-bold">
-        <p>I like to</p>
-        <Typed
-          className="text-red-500 pl-2"
-          strings={[
-            "&#128187; code",
-            "&#127859; cook",
-            "&#8986; collect watches",
-            "&#127947; exercise",
-            "&#128745; travel",
-            "&#127936; watch basketball",
-            "&#127928; play guitar",
-            "&#128253; watch movies",
-            "&#129316; try new foods",
-          ]}
-          typeSpeed={120}
-          backSpeed={140}
-          loop
-        />
+        <p className="mr-2 md:mr-4">I like to</p>
+        <p className="text-blue-500">
+          <Typewriter
+            words={[
+              "code",
+              "cook",
+              "collect watches",
+              "exercise",
+              "travel",
+              "watch basketball",
+              "play guitar",
+              "watch movies",
+              "try new foods",
+            ]}
+          />
+        </p>
       </div>
     </div>
   );
