b2ed4e7a2eef11d9d958156b9d431b19e5120fe6
bucky-badger-gesmer <ajgesmer@comcast.net>
2022-08-22 15:34:02 -0500

    add footer component

---
+++ app/components/Footer.tsx
@@ -0,0 +1,11 @@
+
+const Footer = () => {
+  return (
+    <footer className="bg-blue-500 text-white bottom-0 p-8 text-center">
+      <h2 className="text-l">I made this with Remix!</h2>
+      <p className="text-l">Let's Connect!</p>
+    </footer>
+  );
+};
+
+export default Footer;
