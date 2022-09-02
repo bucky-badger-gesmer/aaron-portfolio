3fc0d3717a5f4d2bb0b82a5ecd284e356d7a407c
bucky-badger-gesmer <ajgesmer@comcast.net>
2022-09-02 11:11:03 -0400

    change Hero description

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -8,8 +8,7 @@ const Hero = () => {
         Hello there! My name is <span className="text-red-500">Aaron</span>.
       </h1>
       <p className="font-bold p-2">
-        I'm a front end software developer, and I love all things related to the
-        web!
+        I'm a software engineer, and I love all things related to front end software development!
       </p>
       <div className="flex justify-center items-center md:text-5xl sm:text-4xl text-xl font-bold">
         <p className="mr-2 md:mr-4">I like to</p>
@@ -54,10 +53,10 @@ const Hero = () => {
         </div>
       </div>
       <div className="container mx-auto p-8 my-8">
-        <h1 className="text-2xl">My Projects</h1>
+        <h1 className="text-2xl m-8" >My Projects</h1>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
-          <div className="flex justify-center text-6xl border-2 border-gray-300 rounded-xl p-6 bg-gray-100 shadow-2xl">
-            1
+          <div className="flex justify-center text-xl border-2 border-gray-300 rounded-xl p-6 bg-gray-100 shadow-2xl">
+            Remix Propublica Congress App
           </div>
           <div className="flex justify-center text-6xl border-2 border-gray-300 rounded-xl p-6 bg-gray-100 shadow-2xl">
             2
